68358ebe0e4b43c5482347bfd758041782e02b9f
cavinluck <2656477405@qq.com>
2021-04-26 16:36:04 +0800

    up 2th

--- src/Test_HelloWorld_demo.java
+++ src/Test_HelloWorld_demo.java
@@ -1,5 +1,7 @@
 public class Test_HelloWorld_demo {
     public static void main(String args[]) {
         System.out.println("Hello, World!");
+        // git test 2th
+        System.out.println("outpu data 2th");
     }
 }
